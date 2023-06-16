525548a5b76b75b93c07808095ffe2dd572a2321
MegaDomo <joshua.villemain@gmail.com>
2023-06-16 15:53:22 -0400

    file management

--- 3D Project/Assets/Testing/Scripts/PlayerHandDrill.cs
+++ 3D Project/Assets/Testing/Scripts/PlayerHandDrill.cs
@@ -4,4 +4,22 @@ using UnityEngine.InputSystem;
 public class PlayerHandDrill : MonoBehaviour
 {
     public InputAction drillAction;
+
+    private void Update()
+    {
+        if (drillAction.IsPressed())
+        {
+            Debug.Log("Being Pressed");
+        }
+    }
+
+    private void OnEnable()
+    {
+        drillAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        drillAction.Disable();
+    }
 }

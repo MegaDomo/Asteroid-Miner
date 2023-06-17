e93aa1e4beeb75172799dfa4f8db8bd391b709b2
AndrewH926 <hagglund.andrew@gmail.com>
2023-06-17 14:04:11 -0400

    loot push

--- 3D Project/Assets/Asteroids/AsteroidScripts/AsteroidLoot.cs
+++ 3D Project/Assets/Asteroids/AsteroidScripts/AsteroidLoot.cs
@@ -6,11 +6,35 @@ public class AsteroidLoot : MonoBehaviour
 {
     [Header("Unity References")]
     public Interaction interaction;
+    public Transform player;
+
+    [Header("Attributes")]
+    public float floatSpeed;
+    public float pickUpRadius;
+    public bool willFloatToPlayer;
+
+    private Vector3 _velocity = Vector3.zero;
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        if ((Vector3.Distance(gameObject.transform.position, player.position) < pickUpRadius) && willFloatToPlayer)
+            transform.position = Vector3.SmoothDamp(transform.position, player.position, ref _velocity, Time.deltaTime * floatSpeed);
+    }
 
     public void PickUpLoot()
     {
         Debug.Log("Loot was picked up");
         gameObject.SetActive(false);
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        PickUpLoot();
     }
 
     private void OnEnable()

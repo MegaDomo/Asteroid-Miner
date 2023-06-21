16a958be5b5ce8839a39f0e9c53b84124bda1535
MegaDomo <joshua.villemain@gmail.com>
2023-06-21 18:02:01 -0400

    yuyp

--- 3D Project/Assets/Testing/Scripts/DockTractorBeam.cs
+++ 3D Project/Assets/Testing/Scripts/DockTractorBeam.cs
@@ -5,44 +5,52 @@ using UnityEngine;
 public class DockTractorBeam : MonoBehaviour
 {
     [Header("Unity References")]
-    public Transform pullPoint;
+    public Transform mainPullPoint;
+    public Transform alternatePullPoint;
 
     [Header("Attributes")]
     public float pullStrength = 1f;
+    public List<LayerMask> layersToPullOn;
+    public List<string> layersToPullOnStrings;
 
     private bool isDocked;
-    private Rigidbody rb;
+    private List<Rigidbody> rbs = new List<Rigidbody>();
 
     private void OnTriggerEnter(Collider collider)
     {
-        if (collider.gameObject.tag == "Vehicle")
-            rb = collider.GetComponent<Rigidbody>();
+        foreach (string mask in layersToPullOnStrings)
+            if (collider.gameObject.tag == mask)
+                rbs.Add(collider.attachedRigidbody);
     }
 
     private void OnTriggerStay(Collider collider)
     {
-        if (collider.gameObject.tag == "Vehicle")
+        foreach (Rigidbody rb in rbs)
         {
-            Vector3 dir = pullPoint.position - collider.transform.position;
+            Vector3 dir = CalculatePullPoint() - rb.transform.position;
 
             // Stops Spasms when on center
             if (dir.magnitude <= 0.1f)
-            {
-                isDocked = true;
-                return;
-            }
-            else
-            {
-                isDocked = false;
-            }
-                
+                continue;
 
             rb.AddForce(dir.normalized * pullStrength, ForceMode.Force);
         }
     }
 
+    public void OnTriggerExit(Collider collider)
+    {
+        rbs.Remove(collider.attachedRigidbody);
+    }
+
     public bool IsDocked()
     {
         return isDocked;
+    }
+
+    private Vector3 CalculatePullPoint()
+    {
+        if (mainPullPoint == null)
+            return alternatePullPoint.position;
+        return mainPullPoint.position;
     }
 }

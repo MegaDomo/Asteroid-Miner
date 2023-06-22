557dc77e75b4a22fec1e1c8102f8dfe139527912
MegaDomo <joshua.villemain@gmail.com>
2023-06-22 15:38:29 -0400

    some bugs

--- 3D Project/Assets/Testing/Scripts/CraneTerminal.cs
+++ 3D Project/Assets/Testing/Scripts/CraneTerminal.cs
@@ -8,14 +8,36 @@ public class CraneTerminal : MonoBehaviour
 
     [Header("Unity References")]
     public Interaction interaction;
+    public CharacterController controller;
     public Camera craneCam;
 
-    [Header("Attribute")]
-    public InputAction exitAction;
+    [Header("Attributes")]
+    public float craneSpeed = 10f;
+
+    private PlayerInput playerInput;
+    private InputAction moveAction;
+    private InputAction exitAction;
 
     private void Awake()
     {
-        exitAction.performed += ctx => { StopInteractingWithTerminal(ctx); };
+        playerInput = new PlayerInput();
+    }
+
+    private void Update()
+    {
+        MoveCrane();
+    }
+
+    private void MoveCrane()
+    {
+        if (playerManager.IsPlayerInControl())
+            return;
+
+        float x = moveAction.ReadValue<Vector3>().x;
+        float z = moveAction.ReadValue<Vector3>().z;
+        Vector3 dir = new Vector3(x, 0, z).normalized;
+        if (dir.magnitude >= 0.05f)
+            controller.Move(dir * craneSpeed * Time.deltaTime);
     }
 
     public void InteractWithTerminal()
@@ -32,13 +54,19 @@ public class CraneTerminal : MonoBehaviour
     {
         interaction.interactAction += InteractWithTerminal;
 
+        moveAction = playerInput.Crane.MoveAction;
+        moveAction.Enable();
+
+        exitAction = playerInput.Crane.ExitInteractable;
         exitAction.Enable();
+        exitAction.performed += StopInteractingWithTerminal;
     }
 
     private void OnDisable()
     {
         interaction.interactAction -= InteractWithTerminal;
 
+        moveAction.Disable();
         exitAction.Disable();
     }
 }

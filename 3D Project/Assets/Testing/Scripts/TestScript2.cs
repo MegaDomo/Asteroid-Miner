793462d7d20d31bf8f46d6c4e794c6ae45f5849e
MegaDomo <joshua.villemain@gmail.com>
2023-07-10 14:12:04 -0400

    mesh stuff

--- 3D Project/Assets/Testing/Scripts/TestScript2.cs
+++ 3D Project/Assets/Testing/Scripts/TestScript2.cs
@@ -13,42 +13,53 @@ public class TestScript2 : MonoBehaviour
     public int xSize = 20;
     public int zSize = 20;
 
+    public float xPerlin = 0.3f;
+    public float zPerlin = 0.3f;
     private void Start()
     {
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
 
-        CreateShape();
+        StartCoroutine(CreateShape());
+    }
+
+    private void Update()
+    {
         UpdateMesh();
     }
 
-    void CreateShape()
+    IEnumerator CreateShape()
     {
         vertices = new Vector3[(xSize + 1) * (zSize + 1)];
         for (int i = 0, z = 0;  z <= zSize;  z++)
         {
             for (int x = 0; x <= xSize; x++, i++)
             {
-                vertices[i] = new Vector3(x, 0, z);
+                float y = Mathf.PerlinNoise(x * xPerlin, z * zPerlin) * 2f;
+                vertices[i] = new Vector3(x, y, z);
             }
         }
 
         int vert = 0;
         int tris = 0;
         triangles = new int[xSize * zSize * 6];
-        for (int x = 0; x < xSize; x++)
+        for (int z = 0; z < zSize; z++)
         {
-            triangles[tris + 0] = vert + 0;
-            triangles[tris + 1] = vert + xSize + 1;
-            triangles[tris + 2] = vert + 1;
-            triangles[tris + 3] = vert + 1;
-            triangles[tris + 4] = vert + xSize + 1;
-            triangles[tris + 5] = vert + xSize + 2;
+            for (int x = 0; x < xSize; x++)
+            {
+                triangles[tris + 0] = vert + 0;
+                triangles[tris + 1] = vert + xSize + 1;
+                triangles[tris + 2] = vert + 1;
+                triangles[tris + 3] = vert + 1;
+                triangles[tris + 4] = vert + xSize + 1;
+                triangles[tris + 5] = vert + xSize + 2;
 
+                vert++;
+                tris += 6;
+                yield return new WaitForSeconds(0.01f);
+            }
             vert++;
-            tris += 6;
         }
-
     }
 
     void UpdateMesh()

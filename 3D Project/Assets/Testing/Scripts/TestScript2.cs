3a346d25d40332c252ca67b392537390a5e678a8
MegaDomo <joshua.villemain@gmail.com>
2023-07-10 14:03:32 -0400

    mesh script testing

---
+++ 3D Project/Assets/Testing/Scripts/TestScript2.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+public class TestScript2 : MonoBehaviour
+{
+    Mesh mesh;
+
+    Vector3[] vertices;
+    int[] triangles;
+
+    public int xSize = 20;
+    public int zSize = 20;
+
+    private void Start()
+    {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        CreateShape();
+        UpdateMesh();
+    }
+
+    void CreateShape()
+    {
+        vertices = new Vector3[(xSize + 1) * (zSize + 1)];
+        for (int i = 0, z = 0;  z <= zSize;  z++)
+        {
+            for (int x = 0; x <= xSize; x++, i++)
+            {
+                vertices[i] = new Vector3(x, 0, z);
+            }
+        }
+
+        int vert = 0;
+        int tris = 0;
+        triangles = new int[xSize * zSize * 6];
+        for (int x = 0; x < xSize; x++)
+        {
+            triangles[tris + 0] = vert + 0;
+            triangles[tris + 1] = vert + xSize + 1;
+            triangles[tris + 2] = vert + 1;
+            triangles[tris + 3] = vert + 1;
+            triangles[tris + 4] = vert + xSize + 1;
+            triangles[tris + 5] = vert + xSize + 2;
+
+            vert++;
+            tris += 6;
+        }
+
+    }
+
+    void UpdateMesh()
+    {
+        mesh.Clear();
+
+        mesh.vertices = vertices;
+        mesh.triangles = triangles;
+
+        mesh.RecalculateNormals();
+    }
+}

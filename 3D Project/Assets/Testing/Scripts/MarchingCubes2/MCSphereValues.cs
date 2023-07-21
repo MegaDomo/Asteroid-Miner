463e9b491a07b5e9a21e4ea82eed3ab01391a90d
MegaDomo <joshua.villemain@gmail.com>
2023-07-21 17:19:10 -0400

    Update MCSphereValues.cs

--- 3D Project/Assets/Testing/Scripts/MarchingCubes2/MCSphereValues.cs
+++ 3D Project/Assets/Testing/Scripts/MarchingCubes2/MCSphereValues.cs
@@ -15,20 +15,9 @@ public class MCSphereValues : MonoBehaviour
         for (int x = 0; x < gridSize - 1; x++) {
             for (int y = 0; y < gridSize - 1; y++) {
                 for (int z = 0; z < gridSize - 1; z++) {
-                    //grid.SetValue(x, y, z, )
+                    float value = Mathf.Abs((gridCenter - new Vector3(x, y, z)).magnitude) - radius;
 
-                    float[] cubeValues = new float[] {
-                        Mathf.Abs((gridCenter - new Vector3(x    , y    , z + 1)).magnitude) - radius,
-                        Mathf.Abs((gridCenter - new Vector3(x + 1, y    , z + 1)).magnitude) - radius,
-                        Mathf.Abs((gridCenter - new Vector3(x + 1, y    , z    )).magnitude) - radius,
-                        Mathf.Abs((gridCenter - new Vector3(x    , y    , z    )).magnitude) - radius,
-                        Mathf.Abs((gridCenter - new Vector3(x    , y + 1, z + 1)).magnitude) - radius,
-                        Mathf.Abs((gridCenter - new Vector3(x + 1, y + 1, z + 1)).magnitude) - radius,
-                        Mathf.Abs((gridCenter - new Vector3(x + 1, y + 1, z    )).magnitude) - radius,
-                        Mathf.Abs((gridCenter - new Vector3(x    , y + 1, z    )).magnitude) - radius,
-                    };
-
-
+                    grid.SetValue(x, y, z, value);
                 }
             }
         }

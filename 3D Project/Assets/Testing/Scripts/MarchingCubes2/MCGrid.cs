ee9a51bb5a8da26e8992d29ac2bbb9ac92944968
MegaDomo <joshua.villemain@gmail.com>
2023-07-21 17:13:51 -0400

    mc

--- 3D Project/Assets/Testing/Scripts/MarchingCubes2/MCGrid.cs
+++ 3D Project/Assets/Testing/Scripts/MarchingCubes2/MCGrid.cs
@@ -2,17 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MCGrid : MonoBehaviour
+public class MCGrid
 {
-    // Start is called before the first frame update
-    void Start()
+    float[,,] pointValues;
+
+    public float GetValue(int x, int y, int z)
     {
-        
+        return pointValues[x, y, z];
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetValue(int x, int y, int z, float value)
     {
-        
+        pointValues[x, y, z] = value;
     }
 }
---
+++ 3D Project/Assets/Testing/Scripts/MarchingCubes2/MCSphereValues.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MCSphereValues : MonoBehaviour
+{
+    MCGrid grid;
+
+    public MCSphereValues(int gridSize, float radius)
+    {
+        grid = new MCGrid();
+
+        Vector3 gridCenter = new Vector3(gridSize / 2, gridSize / 2, gridSize / 2);
+
+        for (int x = 0; x < gridSize - 1; x++) {
+            for (int y = 0; y < gridSize - 1; y++) {
+                for (int z = 0; z < gridSize - 1; z++) {
+                    //grid.SetValue(x, y, z, )
+
+                    float[] cubeValues = new float[] {
+                        Mathf.Abs((gridCenter - new Vector3(x    , y    , z + 1)).magnitude) - radius,
+                        Mathf.Abs((gridCenter - new Vector3(x + 1, y    , z + 1)).magnitude) - radius,
+                        Mathf.Abs((gridCenter - new Vector3(x + 1, y    , z    )).magnitude) - radius,
+                        Mathf.Abs((gridCenter - new Vector3(x    , y    , z    )).magnitude) - radius,
+                        Mathf.Abs((gridCenter - new Vector3(x    , y + 1, z + 1)).magnitude) - radius,
+                        Mathf.Abs((gridCenter - new Vector3(x + 1, y + 1, z + 1)).magnitude) - radius,
+                        Mathf.Abs((gridCenter - new Vector3(x + 1, y + 1, z    )).magnitude) - radius,
+                        Mathf.Abs((gridCenter - new Vector3(x    , y + 1, z    )).magnitude) - radius,
+                    };
+
+
+                }
+            }
+        }
+    }
+
+    public MCGrid GetGrid()
+    {
+        return grid;
+    }
+}
